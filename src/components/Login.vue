72cff014aaff854beb7187e10ca93b68fb840cb5
sezohessen <hu.elshazly@nu.edu.eg>
2022-09-07 10:04:44 +0200

    Login and signup form design

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+export default {
+    setup(){
+        //refs
+        const email         = ref('')
+        const password      = ref('')
+        const handleSubmit  = () => {
+            console.log(email,password)
+        }
+
+        return {email,password,handleSubmit}
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" required placeholder="Display name" v-model="displayName"/>
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity';
+export default {
+    setup(){
+        //refs
+        const displayName   = ref('')
+        const email         = ref('')
+        const password      = ref('')
+        const handleSubmit  = () => {
+            console.log(displayName,email,password)
+        }
+
+        return {displayName,email,password,handleSubmit}
+    }
+};
+</script>
+
+<style>
+</style>
